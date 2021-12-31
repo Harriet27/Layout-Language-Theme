06bb5acf03263e1abf2b8c0ae0caacdaa17e5289
AldrichN <aldrich.neil.hung@gmail.com>
2021-12-31 15:42:46 +0700

    lazy load at router

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,13 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { useRoutes, Navigate } from "react-router-dom";
-import {
-    NotFound404,
-    Language,
-    Theme,
-    BothLT,
-    Antd,
-    FeaturedProducts,
-    Product,
-} from "./pages";
+
+const NotFound404 = lazy(() => import("./pages/404"));
+const Language = lazy(() => import("./pages/language"));
+const Theme = lazy(() => import("./pages/theme"));
+const BothLT = lazy(() => import("./pages/bothLT"));
+const Antd = lazy(() => import("./pages/antd"));
+const FeaturedProducts = lazy(() => import("./pages/featuredProducts"));
+const Product = lazy(() => import("./pages/products"));
 
 export default function Routes() {
     return useRoutes([
@@ -16,30 +15,82 @@ export default function Routes() {
             path: "/library",
             children: [
                 { element: <Navigate to="/library/language" replace /> },
-                { path: "language", element: <Language /> },
-                { path: "theme", element: <Theme /> },
-                { path: "both", element: <BothLT /> },
+                {
+                    path: "language",
+                    element: (
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <Language />
+                        </Suspense>
+                    ),
+                },
+                {
+                    path: "theme",
+                    element: (
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <Theme />
+                        </Suspense>
+                    ),
+                },
+                {
+                    path: "both",
+                    element: (
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <BothLT />
+                        </Suspense>
+                    ),
+                },
             ],
         },
         {
             path: "/extend",
             children: [
-                { path: "antd", element: <Antd /> },
+                {
+                    path: "antd",
+                    element: (
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <Antd />
+                        </Suspense>
+                    ),
+                },
             ],
         },
         {
             path: "/dynamic-routing",
             children: [
-                { path: "featured-products", element: <FeaturedProducts /> },
-                { path: "product/:id", element: <Product /> },
+                {
+                    path: "featured-products",
+                    element: (
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <FeaturedProducts />
+                        </Suspense>
+                    ),
+                },
+                {
+                    path: "product/:id",
+                    element: (
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <Product />
+                        </Suspense>
+                    ),
+                },
             ],
         },
         {
             path: "/",
             children: [
-                { path: "404", element: <NotFound404 /> },
+                {
+                    path: "404",
+                    element: (
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <NotFound404 />
+                        </Suspense>
+                    ),
+                },
             ],
         },
-        { path: '*', element: <Navigate to="/404" replace /> },
+        {
+            path: '*',
+            element: <Navigate to="/404" replace />,
+        },
     ]);
 };
