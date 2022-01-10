77f15e83beaecf3be8a9cad013ee137e405e993a
AldrichN <aldrich.neil.hung@gmail.com>
2022-01-10 10:50:07 +0700

    hmm nice design tho

--- src/pages/reactQuery.js
+++ src/pages/reactQuery.js
@@ -10,8 +10,10 @@ const ReactQuery = () => {
     const { data, status } = useQuery('planets', fetchPlanets);
 
     return (
-        <div>
-            <h1>React Query</h1>
+        <div style={styles.container}>
+            <h1 style={styles.header}>
+                React Query
+            </h1>
 
             {/* Loading */}
             {status === "loading" && <div>Loading...</div>}
@@ -22,17 +24,41 @@ const ReactQuery = () => {
             {/* Success */}
             {status === "success" && (
                 data.results.map(planet => (
-                    <div>
-                        <div><b>{planet.name}</b></div>
-                        <div>Population: {planet.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}</div>
-                        <div>Terrain: {planet.terrain}</div>
-                        <br />
+                    <div style={styles.card}>
+                        <div style={styles.txt}><b>{planet.name}</b></div>
+                        <div style={styles.txt}>Population: {planet.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}</div>
+                        <div style={styles.txt}>Terrain: {planet.terrain}</div>
                     </div>
                 ))
             )}
 
         </div>
     );
+};
+
+const styles = {
+    container: {
+        backgroundColor: '#1d1c1d',
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+    },
+    header: {
+        color: '#f5e100',
+        fontSize: '2rem',
+        fontWeight: 'bold',
+        marginLeft: '1rem',
+    },
+    card: {
+        padding: '0.75rem 1rem',
+        backgroundColor: '#555555',
+        width: '350px',
+        margin: '0 1rem 1rem 1rem',
+        borderRadius: '5px',
+    },
+    txt: {
+        color: 'white',
+    },
 };
 
 export default ReactQuery;
