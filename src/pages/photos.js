68ee5d80e9d6efb1904cc72e4fa847176aa135b1
aldrich.neil.bvt <aldrich.neil@bvarta.com>
2022-01-27 17:22:23 +0700

    render

--- src/pages/photos.js
+++ src/pages/photos.js
@@ -1,12 +1,57 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import MainLayout from "../components/Layouts/MainLayout";
+import { getAllPhotos } from '../redux/actions/photoAction';
 
 const Photos = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getAllPhotos());
+    }, []);
+
+    const photoList = useSelector((state) => state.photo.photosList);
+    const list = photoList.slice(0, 20);
+
+    const renderPhotos = () => {
+        return list.map((val) => (
+            <div
+                key={val.id}
+                style={styles.content}
+            >
+                <div style={{fontWeight: "bold"}}>
+                    {val.title}
+                </div>
+                <div>
+                    {val.url}
+                </div>
+                <div>
+                    <img src={val.thumbnailUrl} alt={val.thumbnail} />
+                </div>
+            </div>
+        ));
+    };
+
     return (
         <MainLayout>
-            photos
+            <div style={styles.root}>
+                {renderPhotos()}
+            </div>
         </MainLayout>
     );
+};
+
+const styles = {
+    root: {
+        marginTop: "1rem",
+    },
+    content: {
+        marginBottom: "1rem",
+        padding: "0.75rem",
+        borderRadius: "5px",
+        width: "25rem",
+        backgroundColor: "ghostwhite",
+    },
 };
 
 export default Photos;
